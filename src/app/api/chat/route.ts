120108f64b0cb83fa3e901793c22514e5d498a76
TrejGun <trejgun@gmail.com>
2025-03-02 13:22:53 +0400

    add moderation of user input

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,8 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import { openai } from "@ai-sdk/openai";
 import { type CoreMessage, streamText, tool } from "ai";
-
+import OpenAI from "openai";
+import { openai } from "@ai-sdk/openai";
 
 const BodyValidationSchema = z.object({
   messages: z.array(
@@ -23,12 +23,23 @@ const formatDate = (date: Date) => date.toLocaleDateString("en-US", {
   second: "numeric",
 });
 
+const openaiInstance = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const messages = BodyValidationSchema.parse(body).messages as CoreMessage[];
+
+  const {success, data} =  BodyValidationSchema.safeParse(body);
+  if (!success) {
+    return NextResponse.json({ error: "Validation failed" }, { status: 400 });
+  }
+
+  const isFlagged = await moderate(data.messages as Array<CoreMessage>);
+  if (isFlagged) {
+    return NextResponse.json({ error: "Moderation failed" }, { status: 400 });
+  }
 
   const stream = streamText({
-    messages,
+    messages: data.messages as Array<CoreMessage>,
     model: openai("gpt-4o-mini"),
     system: `
       Ты — полезный ассистент в приватной клинике.
@@ -36,13 +47,7 @@ export async function POST(req: NextRequest) {
       
       В таблице указаны врачи, работающие в клинике, и их часы приема:
       
-      | Тип             | Имя                   | Понедельник | Вторник | Среда | Четверг | Пятница | Суббота | Воскресенье |
-      |-----------------|-----------------------|-------------|---------|-------|---------|---------|---------|-------------|
-      | DENTIST         | Мария Ивановна        |        9-18 |         |  9-18 |         |    9-18 |         |             |
-      | DENTIST         | Клавдия Сигизмундовна |             |    9-18 |       |    9-18 |         |         |             |
-      | ENT             | Олег Васильевич       |        9-18 |    9-18 |  9-18 |    9-18 |    9-18 |         |             |
-      | OPHTHALMOLOGIST | Надежда Петровна      |        9-18 |    9-18 |  9-18 |    9-18 |    9-18 |         |             |
-      | SURGEON         | Игорь Николаевич      |        9-12 |    9-12 |  9-12 |    9-12 |    9-12 |         |             |
+      ${getDoctorsList()}
       
       Текущая дата ${formatDate(new Date())}, 
       Время приема у врача ограничено одним часом.
@@ -89,3 +94,42 @@ export async function POST(req: NextRequest) {
   });
   return stream.toDataStreamResponse();
 }
+
+const moderate = async (messages: Array<CoreMessage>) => {
+  const {results} = await openaiInstance.moderations.create({
+    input: messages.reduce<Array<string>>((memo, current) => {
+      switch (current.role) {
+        case "user":
+        case "assistant":
+        case "system":
+          if (typeof current.content === "string") {
+            return memo.concat(current.content);
+          } else {
+            return memo.concat(current.content.reduce<Array<string>>((memo, current) => {
+              if (current.type === "text") {
+                return memo.concat(current.text);
+              }
+              return memo;
+            }, []));
+          }
+        case "tool":
+          return memo
+      }
+    }, []),
+  });
+
+  return results.some(result => result.flagged);
+};
+
+
+const getDoctorsList = () => {
+  return `
+    | Тип             | Имя                   | Понедельник | Вторник | Среда | Четверг | Пятница | Суббота | Воскресенье |
+    |-----------------|-----------------------|-------------|---------|-------|---------|---------|---------|-------------|
+    | DENTIST         | Мария Ивановна        |        9-18 |         |  9-18 |         |    9-18 |         |             |
+    | DENTIST         | Клавдия Сигизмундовна |             |    9-18 |       |    9-18 |         |         |             |
+    | ENT             | Олег Васильевич       |        9-18 |    9-18 |  9-18 |    9-18 |    9-18 |         |             |
+    | OPHTHALMOLOGIST | Надежда Петровна      |        9-18 |    9-18 |  9-18 |    9-18 |    9-18 |         |             |
+    | SURGEON         | Игорь Николаевич      |        9-12 |    9-12 |  9-12 |    9-12 |    9-12 |         |             |
+    `;
+};
